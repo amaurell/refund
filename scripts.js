344e820d8d475a044c357bbd1ce79f96a4b90844
Andre Maurell <amaurell@gmail.com>
2025-03-05 13:43:45 -0300

    Adicionando o valor da despesa

--- scripts.js
+++ scripts.js
@@ -73,8 +73,15 @@ function expenseAdd(newExpense) {
     // Adiciona name e category na div
     expenseInfo.append(expenseName, expenseCategory);
 
+    // Cria o valor da despesa
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+      .toUpperCase()
+      .replace("R$", "")}`;
+
     //Adiciona as informações no item
-    expenseItem.append(expenseIcon, expenseInfo);
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount);
 
     //Adiciona o item na lista
     expenseList.append(expenseItem);
